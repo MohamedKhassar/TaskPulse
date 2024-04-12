2e9e5ef318582ed431bb4a20137c29db76a62d57
khassarMohamed <mohamedkhassar775@gmail.com>
2024-04-12 17:04:40 +0000

    fix error of typescript

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,19 +4,11 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import bcrypt from "bcryptjs";
-import { NextAuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
 import User from "@/Models/userModel";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
     CredentialsProvider({
       name: "credentials",
       type: "credentials",
@@ -66,8 +58,8 @@ export const authOptions = {
   pages: {
     signIn: "/",
   }
-} as NextAuthOptions
+}
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as never;
 
 export { handler as GET, handler as POST };
