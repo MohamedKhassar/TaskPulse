725b9c9b656f7447078d580f09af82b61ec8222a
khassarMohamed <mohamedkhassar775@gmail.com>
2024-04-15 00:34:12 +0100

    "Updated Cards component to use task object instead of taskUpdate in render and added setTaskUpdate in deleteTaskById success handler"

--- src/components/ui/Cards.tsx
+++ src/components/ui/Cards.tsx
@@ -31,9 +31,8 @@ const Cards = ({ task, projectId }: { task: Task, projectId: string }) => {
 
     const confirmDelete = async () => {
         try {
-
             await dispatch(deleteTaskById(task._id)).then(async () => {
-                await dispatch(fetchProjectById(projectId)).then(() => setIsClose({ ...isClose, delete: false }))
+                await dispatch(fetchProjectById(projectId)).then(() => setIsClose({ ...isClose, delete: false })).then(() => setTaskUpdate(task))
             })
         } catch (error) {
             console.log(error)
@@ -66,7 +65,7 @@ const Cards = ({ task, projectId }: { task: Task, projectId: string }) => {
         }
     }
     return (
-        <div className={`${taskUpdate.status == TaskStatus.ToDo ? "border-l-[#23B7E5]/40" : taskUpdate.status == TaskStatus.Doing ? "border-l-[#845ADF]/40" : "border-l-[#26BF94]/40"} flex justify-between p-2 w-full bg-[#1A1C1E] h-fit border-l-[7px] rounded-lg ${isDragging && 'opacity-75'}`} ref={setNodeRef}
+        <div className={`${task.status == TaskStatus.ToDo ? "border-l-[#23B7E5]/40" : task.status == TaskStatus.Doing ? "border-l-[#845ADF]/40" : "border-l-[#26BF94]/40"} flex justify-between p-2 w-full bg-[#1A1C1E] h-fit border-l-[7px] rounded-lg ${isDragging && 'opacity-75'}`} ref={setNodeRef}
             {...attributes}
             style={{
                 transition,
@@ -75,17 +74,17 @@ const Cards = ({ task, projectId }: { task: Task, projectId: string }) => {
             <div className='flex flex-col gap-y-5'>
                 <div className='flex gap-x-2 items-center'>
                     <Star className="fill-white/50 border-none" strokeWidth={0.2} size={13} />
-                    <h1 className='font-semibold text-xs font-Poppins text-white/70'  {...listeners}>{taskUpdate.taskName}</h1>
+                    <h1 className='font-semibold text-xs font-Poppins text-white/70'  {...listeners}>{task.taskName}</h1>
                 </div>
                 <div>
                     <ul className='flex flex-col gap-y-3 font-normal text-xs font-Poppins text-white/70'>
-                        <li>Assigned On : <span className='text-[12px] text-white/50'>{new Date(taskUpdate.created_at).toLocaleDateString()}</span></li>
-                        <li>Target Date : <span className='text-[12px] text-white/50'>{new Date(taskUpdate.deadline).toLocaleDateString()}</span></li>
+                        <li>Assigned On : <span className='text-[12px] text-white/50'>{new Date(task.created_at).toLocaleDateString()}</span></li>
+                        <li>Target Date : <span className='text-[12px] text-white/50'>{new Date(task.deadline).toLocaleDateString()}</span></li>
                         <li className='relative flex items-center gap-x-1'>
                             Assigned To :
                             <div className='flex gap-x-1 items-center' onClick={() => setSearchMembers(!searchMembers)}>
 
-                                {taskUpdate.assignedTo && <AnimatedTooltip items={taskUpdate.assignedTo} />}
+                                {task.assignedTo && <AnimatedTooltip items={task.assignedTo} />}
 
                                 <UserPlus size={23} color='black' className='hover:bg-white/30 duration-300 cursor-pointer bg-white/50 rounded-full p-1' />
                             </div>
@@ -104,17 +103,17 @@ const Cards = ({ task, projectId }: { task: Task, projectId: string }) => {
                         data-testid="delete-button" className='bg-[#E6533C]/10 hover:bg-[#E6533C]/40 duration-300 rounded-lg p-2' onClick={() => setIsClose({ ...isClose, delete: true })}><Trash className='size-3' color='#E6533C' /></button>
                 </div>
                 <div className='flex justify-end relative' onClick={() => setShowPriority(!showPriority)}>
-                    <h1 className={`${taskUpdate.priority == TaskPriority.Medium ? "bg-[#F5B849]/10 hover:bg-[#F5B849]/40 text-[#F5B849]" : taskUpdate.priority == TaskPriority.High ? "bg-[#E6533C]/10 hover:bg-[#E6533C]/40 text-[#E6533C]" : "bg-[#26BF94]/10 hover:bg-[#26BF94]/40 text-[#26BF94]"}  w-fit py-1 px-2 text-center font-semibold rounded-[4px] text-[11px] duration-300 capitalize`}>{taskUpdate.priority}</h1>
+                    <h1 className={`${task.priority == TaskPriority.Medium ? "bg-[#F5B849]/10 hover:bg-[#F5B849]/40 text-[#F5B849]" : task.priority == TaskPriority.High ? "bg-[#E6533C]/10 hover:bg-[#E6533C]/40 text-[#E6533C]" : "bg-[#26BF94]/10 hover:bg-[#26BF94]/40 text-[#26BF94]"}  w-fit py-1 px-2 text-center font-semibold rounded-[4px] text-[11px] duration-300 capitalize`}>{task.priority}</h1>
                     {showPriority &&
                         <div className='flex gap-x-5 absolute top-7 bg-slate-800 rounded-md p-3 z-10'>
                             <button className={cn("bg-[#E6533C]/10 hover:bg-[#E6533C]/40 text-[#E6533C]  w-fit py-1 px-2 text-center font-semibold rounded-[4px] text-[11px] duration-300 capitalize",
-                                TaskPriority.High === taskUpdate.priority && "hidden"
+                                TaskPriority.High === task.priority && "hidden"
                             )} onClick={() => { setIsClose({ ...isClose, update: true }); setTaskUpdate({ ...taskUpdate, priority: TaskPriority.High }) }}>{TaskPriority.High}</button>
                             <button className={cn("bg-[#F5B849]/10 hover:bg-[#F5B849]/40 text-[#F5B849] w-fit py-1 px-2 text-center font-semibold rounded-[4px] text-[11px] duration-300 capitalize",
-                                TaskPriority.Medium === taskUpdate.priority && "hidden"
+                                TaskPriority.Medium === task.priority && "hidden"
                             )} onClick={() => { setIsClose({ ...isClose, update: true }); setTaskUpdate({ ...taskUpdate, priority: TaskPriority.Medium }) }}>{TaskPriority.Medium}</button>
                             <button className={cn("bg-[#26BF94]/10 hover:bg-[#26BF94]/40 text-[#26BF94]  w-fit py-1 px-2 text-center font-semibold rounded-[4px] text-[11px] duration-300 capitalize",
-                                TaskPriority.Low === taskUpdate.priority && "hidden"
+                                TaskPriority.Low === task.priority && "hidden"
                             )} onClick={() => { setIsClose({ ...isClose, update: true }); setTaskUpdate({ ...taskUpdate, priority: TaskPriority.Low }) }}>{TaskPriority.Low}</button>
 
                         </div>}
