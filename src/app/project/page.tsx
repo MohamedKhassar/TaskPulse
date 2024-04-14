4e32626ca01bf16966856eb7a091776b9aca823d
khassarMohamed <mohamedkhassar775@gmail.com>
2024-04-14 17:17:34 +0100

    "Update scrollbar style in globals.css, add scroll to project table and update table rendering in project page"
    This commit updates the scrollbar style in globals.css by adding a height property to the scroll::-webkit

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -86,62 +86,64 @@ function page() {
                         <div className="w-full">
                             <div className="overflow-auto lg:overflow-visible flex flex-col gap-y-6">
                                 <Form />
-                                <table className="table text-[#7A54CC] border-separate space-y-6 text-md w-full text-center font-body">
-                                    <thead className="bg-gray-800 text-gray-500 text-center capitalize">
-                                        <tr>
-                                            <th className="p-3">name</th>
-                                            <th className="p-3">manager</th>
-                                            <th className="p-3">completion</th>
-                                            <th className="p-3">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className='overflow-y-scroll' style={{ height: "50vh" }}>
-                                        {Array.isArray(projects) ? projects.map(project => (
-                                            <tr key={project._id} className="bg-gray-800 hover:underline">
-                                                <td className="p-3 hover:underline cursor-pointer" onClick={() => router.push(`project/${project._id}`)}>
-                                                    {project.title}
-                                                </td>
-                                                <td className="p-3 font-bold capitalize cursor-pointer" onClick={() => router.push(`project/${project._id}`)}>
-                                                    {project?.userId?.name}
-                                                </td>
-                                                <td className="p-3">
-                                                    <div className="relative p-4 max-w-sm mx-auto">
-                                                        <div className="flex mb-2 items-center justify-between">
-                                                            <div className="text-right">
-                                                                <span className={cn(`w-[${project.tasks && percentage(project.tasks.filter(task => task.status === TaskStatus.Done), project.tasks)}%] rounded-full text-xs font-semibold inline-block`, {
-                                                                    'text-red-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 0 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 25,
-                                                                    'text-yellow-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 25 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 75,
-                                                                    'text-green-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 75 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) <= 100,
-                                                                })}>
-                                                                    {project.tasks && percentage(project.tasks.filter(task => task.status == TaskStatus.Done), project.tasks)}%
-                                                                </span>
+                                <div className='h-[60vh] overflow-scroll scroll'>
+                                    <table className="table text-[#7A54CC] border-separate space-y-6 text-md w-full text-center font-body">
+                                        <thead className="bg-gray-800 text-gray-500 text-center capitalize">
+                                            <tr>
+                                                <th className="p-3">name</th>
+                                                <th className="p-3">manager</th>
+                                                <th className="p-3">completion</th>
+                                                <th className="p-3">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {Array.isArray(projects) ? projects.map(project => (
+                                                <tr key={project._id} className="bg-gray-800 hover:underline">
+                                                    <td className="p-3 hover:underline cursor-pointer" onClick={() => router.push(`project/${project._id}`)}>
+                                                        {project.title}
+                                                    </td>
+                                                    <td className="p-3 font-bold capitalize cursor-pointer" onClick={() => router.push(`project/${project._id}`)}>
+                                                        {project?.userId?.name}
+                                                    </td>
+                                                    <td className="p-3">
+                                                        <div className="relative p-4 max-w-sm mx-auto">
+                                                            <div className="flex mb-2 items-center justify-between">
+                                                                <div className="text-right">
+                                                                    <span className={cn(`w-[${project.tasks && percentage(project.tasks.filter(task => task.status === TaskStatus.Done), project.tasks)}%] rounded-full text-xs font-semibold inline-block`, {
+                                                                        'text-red-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 0 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 25,
+                                                                        'text-yellow-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 25 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 75,
+                                                                        'text-green-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 75 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) <= 100,
+                                                                    })}>
+                                                                        {project.tasks && percentage(project.tasks.filter(task => task.status == TaskStatus.Done), project.tasks)}%
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <div className="flex rounded-full h-2 bg-gray-200">
+                                                                <div className={cn(`w-[${project.tasks && percentage(project.tasks.filter(task => task.status === TaskStatus.Done), project.tasks)}%] rounded-full`, {
+                                                                    'bg-red-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 0 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 25,
+                                                                    'bg-yellow-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 25 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 75,
+                                                                    'bg-green-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 75 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) <= 100,
+                                                                })}></div>
+
                                                             </div>
                                                         </div>
-                                                        <div className="flex rounded-full h-2 bg-gray-200">
-                                                            <div className={cn(`w-[${project.tasks && percentage(project.tasks.filter(task => task.status === TaskStatus.Done), project.tasks)}%] rounded-full`, {
-                                                                'bg-red-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 0 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 25,
-                                                                'bg-yellow-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 25 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) < 75,
-                                                                'bg-green-500': percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) >= 75 && percentage(project.tasks!.filter(task => task.status === TaskStatus.Done), project.tasks!) <= 100,
-                                                            })}></div>
-
+                                                    </td>
+                                                    <td className="p-3">
+                                                        <div className='flex gap-x-6 items-center justify-center'>
+                                                            <button className="inline-flex items-center px-4 py-2 bg-yellow-600/70 hover:bg-yellow-700/70 text-white text-sm font-medium rounded-md" onClick={() => handelUpdate(project._id)}>
+                                                                <Pen size={15} className='text-yellow-400' />
+                                                            </button>
+                                                            <button className="inline-flex items-center px-4 py-2 bg-red-600/70 hover:bg-red-700/70 text-white text-sm font-medium rounded-md" onClick={() => handelAlert(project._id)}>
+                                                                <Trash2 size={15} className='text-red-400' />
+                                                            </button>
                                                         </div>
-                                                    </div>
-                                                </td>
-                                                <td className="p-3">
-                                                    <div className='flex gap-x-6 items-center justify-center'>
-                                                        <button className="inline-flex items-center px-4 py-2 bg-yellow-600/70 hover:bg-yellow-700/70 text-white text-sm font-medium rounded-md" onClick={() => handelUpdate(project._id)}>
-                                                            <Pen size={15} className='text-yellow-400' />
-                                                        </button>
-                                                        <button className="inline-flex items-center px-4 py-2 bg-red-600/70 hover:bg-red-700/70 text-white text-sm font-medium rounded-md" onClick={() => handelAlert(project._id)}>
-                                                            <Trash2 size={15} className='text-red-400' />
-                                                        </button>
-                                                    </div>
-                                                </td>
-                                                {isUpdate && <Update project={project} onClose={() => handelUpdate("")} />}
-                                            </tr>
-                                        ))
-                                            : <tr className='col-span-4'><td className="p-3">not found</td></tr>}</tbody>
-                                </table>
+                                                    </td>
+                                                    {isUpdate && <Update project={project} onClose={() => handelUpdate("")} />}
+                                                </tr>
+                                            ))
+                                                : <tr className='col-span-4'><td className="p-3">not found</td></tr>}</tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextMiddleware, NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
     const protectedRoutes = ["/profile", "/project"];
     const authRoutes = ["/login", "/register"];
-    const currentUser = request.cookies.get('__Secure-next-auth.session-token')?.value;
+    const currentUser = request.cookies.get(`${process.env.TOKEN}`)?.value;
 
     if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
         return NextResponse.redirect(new URL("/profile", request.url).toString());
